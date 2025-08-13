aa2989af4de3dc8b6b6dda5dde3058e1f7d728d9
Cauê Ferreira Lacerda <cauef.lacerda@gmail.com>
2025-08-13 12:39:15 -0300

    learn(ap01-js): encerra o codigo 1.4.1

---
+++ js/aula1.js
@@ -0,0 +1,37 @@
+// comparação
+// operadores == e ===
+
+//coerção
+// const n1 = 2
+// const n2 = '3'
+// //coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+//sistemas de tipos
+// let idade = 20 
+// console.log(typeof(idade))
+// let nome = "Ana"
+// console.log(typeof(nome))
+// idade = "vinte"
+// console.log(typeof(idade))
+// declaração de variáveis
+// const, let e var
+// var linguaguem = "Javascript"
+// console.log("Aprendendo " + linguaguem)
+// var linguaguem = "Java"
+// console.log("Aprendendo " + linguaguem)
+// var c = 2
+// var nome = 'José'
+
+// let a = 2
+// let nome = "José"
+// console.log(a)
+// console.log(nome)
+// nome = "José da Silva"
+// console.log(nome)
+
+//const nome = 'José'
+//console.log(nome)
